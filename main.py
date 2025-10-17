85c510df9f384b5304f2592aa8aa2cb87f5611db
operations@inventzia.com <operations@inventzia.com>
2025-10-17 09:04:57 +0000

    first commit

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ app.layout = layout
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8060, debug=True)
+    app.run(host='0.0.0.0', port=8070, debug=True)
